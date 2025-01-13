9576234eb0eb7bca508bd3ba09456a70d8f3b21e
dineshkumarkarn <132733647+dineshkumarkarn@users.noreply.github.com>
2025-01-13 13:45:58 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+from flask import Flask, render_template, request, jsonify
+
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+
+def get_Chat_response(text):
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+if __name__ == "__main__":
+    while True:
+        
+        user_input = str(input("Tell me what you need an idea for: "))
+        if user_input.lower() in ['exit', 'quit']:
+            break
+        prompt = f"Suggest an idea for {user_input}"
+        idea_suggestion = get_Chat_response(prompt)
+        print(f"Idea: {idea_suggestion}")
